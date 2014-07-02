e221886d70d964adf80bddc529576e34ab7a11ce
Hack Reactor Students <tyler.a.ochiai@gmail.com>
2014-07-02 16:16:02 -0700

    Implements LinksController per spec

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,13 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+  $scope.getLinks = function() {
+    Links.getLinks().then(function(linkData){
+      $scope.data.links = linkData;
+    }).catch(function (error) {
+      console.error(error);
+    });
+  };
+  $scope.getLinks();
 });
